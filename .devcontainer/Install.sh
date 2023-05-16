4641762492bc860d0d16e2850a700428b94241ce
S41L0R <60116462+S41L0R@users.noreply.github.com>
2023-05-16 16:58:50 -0400

    Create Install.sh

---
+++ .devcontainer/Install.sh
@@ -0,0 +1,6 @@
+sudo apt install -y software-properties-common
+
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+sudo apt-add-repository "deb http://apt.llvm.org/buster/ llvm-toolchain-buster main"
+sudo apt-get update
+sudo apt-get install -y clang
