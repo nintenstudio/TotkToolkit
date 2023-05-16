f28c61d1394eea79a14ea6e44aab4e290ceb4338
S41L0R <60116462+S41L0R@users.noreply.github.com>
2023-05-16 17:05:47 -0400

    Update Install.sh

--- .devcontainer/Install.sh
+++ .devcontainer/Install.sh
@@ -4,3 +4,5 @@ wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-add-repository "deb http://apt.llvm.org/buster/ llvm-toolchain-buster main"
 sudo apt-get update
 sudo apt-get install -y clang
+
+git submodule update --init --recursive
