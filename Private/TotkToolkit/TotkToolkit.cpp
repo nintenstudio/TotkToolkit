68021f330f5028d004aeae468c4ffb0c45576a48
S41L0R <60116462+S41L0R@users.noreply.github.com>
2023-05-15 20:28:42 -0400

    Compiles for switch!

---
+++ Private/TotkToolkit/TotkToolkit.cpp
@@ -0,0 +1,26 @@
+﻿#include <TotkToolkit/TotkToolkit.h>
+
+#include <iostream>
+#if SWITCH
+#include <switch.h>
+#endif
+
+int main()
+{
+#if SWITCH
+	consoleInit(NULL);
+
+	std::cout << "Hello world!" << std::endl;
+
+	while (appletMainLoop()) {      // while the application hasn't received an exit request...
+		consoleUpdate(NULL);        // Update the screen
+	}
+
+	consoleExit(NULL);
+
+#endif
+
+	std::cout << "Hello world!" << std::endl;
+
+	return 0;
+}
