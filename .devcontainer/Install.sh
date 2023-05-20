6f72639d4d11d8e768328700af9778d50d7be7e3
S41L0R <60116462+S41L0R@users.noreply.github.com>
2023-05-20 16:37:37 -0400

    Update Install.sh

--- .devcontainer/Install.sh
+++ .devcontainer/Install.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y clang
 
 chmod +x ./.devcontainer/install-devkitpro-pacman.sh
 sudo ./.devcontainer/install-devkitpro-pacman.sh
-printf "\n" "y" | sudo dkp-pacman -S switch-dev
+printf "\n y" | sudo dkp-pacman -S switch-dev
 export DEVKITPRO=/opt/devkitpro
 
 git submodule update --init --recursive
