b37dad690e5c74238eb0c4d4f3a87e74479fdf38
S41L0R <60116462+S41L0R@users.noreply.github.com>
2023-05-20 15:28:30 -0400

    Update Install.sh

--- .devcontainer/Install.sh
+++ .devcontainer/Install.sh
@@ -3,8 +3,8 @@ sudo apt install -y software-properties-common
 sudo apt-get update
 sudo apt-get install -y clang
 
-chmod +x ./install-devkitpro-pacman.sh
-sudo ./install-devkitpro-pacman.sh
+chmod +x ./.devcontainer/install-devkitpro-pacman.sh
+sudo ./.devcontainer/install-devkitpro-pacman.sh
 sudo dkp-pacman -S switch-dev
 
 git submodule update --init --recursive
