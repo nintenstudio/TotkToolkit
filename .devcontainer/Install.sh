a0b002f93c25559057f0bc4ac9cbbd0b8620847e
S41L0R <60116462+S41L0R@users.noreply.github.com>
2023-05-20 16:09:51 -0400

    more codespaces stuff

--- .devcontainer/Install.sh
+++ .devcontainer/Install.sh
@@ -5,6 +5,7 @@ sudo apt-get install -y clang
 
 chmod +x ./.devcontainer/install-devkitpro-pacman.sh
 sudo ./.devcontainer/install-devkitpro-pacman.sh
-printf "\n" | sudo dkp-pacman -S switch-dev
+printf "\n" "y" | sudo dkp-pacman -S switch-dev
+export DEVKITPRO=/opt/devkitpro
 
 git submodule update --init --recursive
