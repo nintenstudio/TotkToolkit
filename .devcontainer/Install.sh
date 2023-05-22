6e25f235c202dc22b64b226154da0d7debdd5245
S41L0R <60116462+S41L0R@users.noreply.github.com>
2023-05-22 00:02:10 +0000

    Renders spinning triangle

--- .devcontainer/Install.sh
+++ .devcontainer/Install.sh
@@ -6,6 +6,14 @@ sudo apt-get install -y clang
 chmod +x ./.devcontainer/install-devkitpro-pacman.sh
 sudo ./.devcontainer/install-devkitpro-pacman.sh
 printf "\n y" | sudo dkp-pacman -S switch-dev
+printf "\n y" | sudo dkp-pacman -S switch-mesa
 echo 'export DEVKITPRO=/opt/devkitpro' >> ~/.bashrc
+
+sudo apt install libxrandr-dev -y
+sudo apt install libxinerama-dev -y
+sudo apt install libxcursor-dev -y
+sudo apt install libxi-dev -y
+sudo apt install libgl1-mesa-dev -y
+sudo apt install libegl1-mesa-dev -y
 
 git submodule update --init --recursive
