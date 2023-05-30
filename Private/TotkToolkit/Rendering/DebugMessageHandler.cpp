5cf418f135cf30caf1102c350de2ed05a47b5915
S41L0R <60116462+S41L0R@users.noreply.github.com>
2023-05-30 14:28:40 -0400

    Log to file

--- Private/TotkToolkit/Rendering/DebugMessageHandler.cpp
+++ Private/TotkToolkit/Rendering/DebugMessageHandler.cpp
@@ -2,6 +2,7 @@
 
 #include <glad/glad.h>
 #include <iostream>
+#include <fstream>
 
 void GLAPIENTRY
 DebugMessageCallback(GLenum source,
@@ -13,7 +14,10 @@ DebugMessageCallback(GLenum source,
     const void* userParam)
 {
     if (severity == GL_DEBUG_SEVERITY_MEDIUM || severity == GL_DEBUG_SEVERITY_HIGH) {
-        std::fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+        std::fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n", // To log
+            (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+            type, severity, message);
+        std::fprintf(stdout, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n", // To console
             (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
             type, severity, message);
     }
@@ -22,6 +26,7 @@ DebugMessageCallback(GLenum source,
 namespace TotkToolkit::Rendering {
 	void DebugMessageHandler::Init() {
 #ifndef NDEBUG
+        std::freopen("TotkToolkit.log", "w", stderr);
 		glEnable(GL_DEBUG_OUTPUT);
 		glDebugMessageCallback(DebugMessageCallback, 0);
 #endif
